0cb8e3ba7ab147ea13aa578d6af477b97429b791
littleblacksheep <hanzhe@uw.edu>
2021-05-19 13:03:02 -0700

    fix vision mean pooler dim

--- src/lxrt/modeling.py
+++ src/lxrt/modeling.py
@@ -597,7 +597,7 @@ class BertMeanPooler(nn.Module):
 class VisionMeanPooler(nn.Module):
     def __init__(self, config):
         super(BertMeanPooler, self).__init__()
-        self.dense = nn.Linear(VISUAL_CONFIG.visual_feat_dim, config.hidden_size)
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.activation = nn.ReLU()
 
     def forward(self, hidden_states):
